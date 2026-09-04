788e2406df0c619fe610a3656cdef3d2967f0b8e
basty <bast.salazarm@duocuc.cl>
2026-09-03 21:29:32 -0400

    JAVA-006 - Implementar Main y crear Estudiante

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
     //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    Estudiante estudiante = new Estudiante("ana", "ingeniera en informatica", 20);
+    public class main {
+        public static void main(String[] args) {
 
-    estudiante.mostrarInformacion();
+            Estudiante estudiante = new Estudiante("ana", "ingeniera en informatica", 20);
+
+            estudiante.mostrarInformacion();
+        }
+    }
 }
