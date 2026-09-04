00bf84cbd7f2b300a1e090c596ec7f958d2509f7
basty <bast.salazarm@duocuc.cl>
2026-09-04 19:53:54 -0400

    JAVA-011 - Agregar menu con while

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,27 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            for (int contador = 1; contador < 5; contador++){
-                System.out.println("registro procesado: "+ contador);
+            for (int contador = 1; contador < 5; contador++) {
+                System.out.println("registro procesado: " + contador);
+
+                int opcion = 1;
+                while (opcion != 0) {
+
+                    System.out.println("------MENU-------");
+                    System.out.println("1). Estado");
+                    System.out.println("2). Operaciones");
+                    System.out.println("3). Salir");
+
+                    System.out.println("Eliga la opcion");
+                    opcion = Scanner.nextInt();
+                }
+                if (opcion == 1) ;
+                System.out.println("Sistema operativo");
+            } else if (opcion == 2) {
+                System.out.println("Proceso operacional");
+            } else if (opcion == 0) {
+                System.out.println("Cerrando sistema");
+
 
             Scanner scanner = new Scanner(System.in);
 
